e36e864290404bb3cad554b918ba348b4de82d47
Chris Z <eathena@hotmail.com>
2018-05-23 09:12:36 -0400

    Initial commit

---
+++ src/com/chrisz/Vehicle.java
@@ -0,0 +1,67 @@
+package com.chrisz;
+
+public class Vehicle {
+    private String name;
+    private String size;
+
+    private int currentVelocity;
+    private int currentDirection;
+
+    public Vehicle(String name, String size) {
+        this.name = name;
+        this.size = size;
+
+        this.currentVelocity = 0;
+        this.currentDirection = 0;
+    }
+
+    public void steer(int direction) {
+        this.currentDirection += direction;
+        System.out.println("Vehicle.steer(): Steering at" + currentDirection + " degrees.");
+    }
+
+    public void move(int velocity, int direction) {
+        // note can write this.currentVelocity but optional here
+        // but if the class variable was called velocity intesd of currentVelocity and parameter was called velocity
+        // than the word this.velocity would be required
+        currentVelocity = velocity;
+        currentDirection = direction;
+        System.out.println("Vehicle.move(): Moving at " + currentVelocity + " in direction " + currentDirection);
+    }
+
+    public void stop() {
+        this.currentVelocity = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getCurrentVelocity() {
+        return currentVelocity;
+    }
+
+    public void setCurrentVelocity(int currentVelocity) {
+        this.currentVelocity = currentVelocity;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+}
